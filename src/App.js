71dc0fa919ab1afb388c78381386c539ecce9be6
tonkla785 <tonkla785@gmail.com>
2024-08-05 10:14:05 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+
+export default function App(){
+  return(
+    <>
+    <h2>DeveloperThai.com</h2>
+    <div className='title'>React & React Native</div>
+    </>
+  )
+}
