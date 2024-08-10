e0c14d8dfd097f3f84ab3a5f074bb7f4858a4a31
tonkla785 <tonkla785@gmail.com>
2024-08-11 00:50:44 +0700

    add func

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Calendar from './class-components'
+import { Header,Content,Footer } from './func-components'
 
 export default function App(){
   return(
     <>
-    <p><center><Calendar/></center></p>
+    <Header/>
+    <Content/>
+    <Footer/>
     </>
   )
 }
